476b3decad7168f112aed3e34f0b32a4897f39a2
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-05-29 19:52:18 +0300

    project added

---
+++ travel_agency/static/assets/js/app.js
@@ -0,0 +1,63 @@
+// home section image changes
+
+let home = document.getElementById('home');
+
+let title = home.querySelector('.home-title');
+
+let subtitle = home.querySelector('.home-desc');
+
+let source_image = ['/static/assets/css/bg/0.jpg', '/static/assets/css/bg/2.jpg', '/static/assets/css/bg/3.jpg', '/static/assets/css/bg/4.jpg', '/static/assets/css/bg/5.jpg', '/static/assets/css/bg/6.jpg'];
+
+let _title = ["O'zbek madaniy merosi", "O'zbek me'morchiligi", "Xalq san'ati"];
+let _subtitle = [
+    "O‘zbekiston madaniyati, Markaziy Osiyo xalqlarining ko‘p asrlik an’analari va turmush tarzi bilan chambarchas bog‘liq boy tarixga ega",
+    "Me’morchilik ijodining gullab-yashnashi O‘zbekiston Buyuk Ipak yo‘lining markazi bo‘lgan o‘rta asrlar davriga to‘g‘ri keladi. XIV asrdan boshlab bu yerda dunyoga mashhur me’morchilik yodgorliklari – Samarqanddagi Registon maydoni, Shohi-Zinda majmuasi, Bibixonim masjidi va Gur-Amir maqbarasi, Shahrisabzdagi Oqsaroy, Buxorodagi Poyi kalon va Labi Hovuz, Xivada minora va madrasalar barpo etildi.",
+    "O‘zbekistonda azaldan noyob iste’dodli odamlar yashab, xalq san’atining  nodir an’analarini asrab-avaylab, avloddan-avlodga yetkazib kelmoqdalar. Ushbu an’analar bizning tariximizni, ajoyib madaniyatimiz va milliy qadriyatlarimizni unutishga yo‘l bermaydi.",
+];
+
+setInterval(() => {
+    let random_image = source_image[Math.floor(Math.random() * source_image.length)]
+    let random_title = _title[Math.floor(Math.random() * _title.length)]
+    let random_subtitle = _subtitle[Math.floor(Math.random() * _subtitle.length)]
+    setTimeout(() => {
+        home.style = `background-image: url(${random_image}) !important;`;
+        title.attributes['data-title'].nodeValue = random_title;
+        subtitle.attributes['data-title'].nodeValue = random_subtitle;
+    }, 1000)
+}, 3000)
+
+
+function scrollTrigger() {
+    let navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 133) {
+            navbar.classList.add('scroll');
+        } else {
+            navbar.classList.remove('scroll');
+        }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+
+        });
+    });
+
+});
+
+scrollTrigger();
---
+++ travel_agency/travel_agency/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+    path('', include('main.urls', namespace='main')),
+
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
